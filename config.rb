5b7eb60bf1afa011cf1659c42146c430694e2757
ltackett <lorin.tackett@gmail.com>
2010-04-26 16:57:07 -0700

    display-box'd

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 ROOT = File.join(File.dirname(__FILE__), '/')
 puts "Site root is: " + File.expand_path(ROOT)
  
-sass_options.output_style = :compressed
-sass_options.project_path = ROOT           # must be set for Compass to work
-sass_options.http_path    = "/"            # root when deployed 
-sass_options.sass_dir     = "sass"         # dir containing Sass / Compass source files
-sass_options.css_dir      = "stylesheets"  # final CSS
-sass_options.images_dir   = "images"       # final images
+output_style = :nested
+project_path = ROOT               # must be set for Compass to work 
+sass_dir     = "sass"  # dir containing Sass / Compass source files
+http_path    = "/"                # root when deployed
+css_dir      = "stylesheets" # final CSS
+images_dir   = "images"      # final images
