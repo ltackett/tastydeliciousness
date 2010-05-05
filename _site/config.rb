ae1d7af2bbd9705f7a0fd730c3cc170422aabf15
ltackett <lorin.tackett@gmail.com>
2010-05-05 12:42:01 -0700

    removed the _site folder and added it to the gitignore file

--- _site/config.rb
+++
@@ -1,9 +0,0 @@
-ROOT = File.join(File.dirname(__FILE__), '/')
-puts "Site root is: " + File.expand_path(ROOT)
- 
-output_style = :nested
-project_path = ROOT               # must be set for Compass to work 
-sass_dir     = "sass"  # dir containing Sass / Compass source files
-http_path    = "/"                # root when deployed
-css_dir      = "stylesheets" # final CSS
-images_dir   = "images"      # final images
